397d84339b8031021587c23862bc503deb1df1b2
rushi1225 <rushibhingare25@gmail.com>
2024-12-12 17:01:38 +0530

    day 10

---
+++ day10/function/script.js
@@ -0,0 +1,17 @@
+//function decleration
+
+function doSomthing() {
+  console.log("hii this is rushikesh");
+}
+
+//function expression
+
+const doSomeTask = function (num1, num2) {
+  console.log(num1 + num2);
+};
+
+//arrow function
+
+const doSomeNeew = () => {
+  console.log("hii this is js arrow function");
+};
